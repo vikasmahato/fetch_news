8a3cbe92fa945ea8a55df62871df2ed6b4501219
yisoftwaredeveloper <yisoftwaredeveloper@youngman.co.in>
2025-03-28 08:04:18 +0530

    Save News Articles

--- fetch_news.py
+++ fetch_news.py
@@ -155,41 +155,40 @@ def lambda_handler(event):
 if __name__ == "__main__":
 
     data = {
-        # 'tourism': {
-        #     "category": "tourism",
-        #     "nisee_category": "travel",
-        #     "sub_categories": ["africa", "europe", "china", "india", "united states", "middle east", "australia"]
-        # },
-        # 'real_estate': {
-        #     "nisee_category": "real_estate",
-        #     "sub_categories": ["real estate"]
-        # },
-        # 'business': {
-        #     "category": "business",
-        #     "nisee_category": "economy",
-        #     "sub_categories": ["market", "economy", "united states", "china", "business"]
-        # },
-        # 'technology': {
-        #     "category": "technology",
-        #     "nisee_category": "technology",
-        #     "sub_categories": ["internet", "gadgets", "software", "mobile", "desktop", "artificial intelligence"]
-        # },
-        # 'sports': {
-        #     "category": "sports",
-        #     "nisee_category": "sports",
-        #     "sub_categories": ["cricket", "hockey", "tennis", "football", "badminton", "basketball", "f1 racing"]
-        # },
-        # 'health': {
-        #     "category": "health",
-        #     "nisee_category": "health",
-        #     "sub_categories": ["nutrition", "mental health", "fitness", "science", "health"]
-        # },
-        # 'politics': {
-        #     "category": "politics",
-        #     "nisee_category": "politics",
-        #     "sub_categories": ["united states", "india", "china", "russia", "europe", "asia", "africa"]
-        # },
-        #
+        'tourism': {
+            "category": "tourism",
+            "nisee_category": "travel",
+            "sub_categories": ["africa", "europe", "china", "india", "united states", "middle east", "australia"]
+        },
+        'real_estate': {
+            "nisee_category": "real_estate",
+            "sub_categories": ["real estate"]
+        },
+        'business': {
+            "category": "business",
+            "nisee_category": "economy",
+            "sub_categories": ["market", "economy", "united states", "china", "business"]
+        },
+        'technology': {
+            "category": "technology",
+            "nisee_category": "technology",
+            "sub_categories": ["internet", "gadgets", "software", "mobile", "desktop", "artificial intelligence"]
+        },
+        'sports': {
+            "category": "sports",
+            "nisee_category": "sports",
+            "sub_categories": ["cricket", "hockey", "tennis", "football", "badminton", "basketball", "f1 racing"]
+        },
+        'health': {
+            "category": "health",
+            "nisee_category": "health",
+            "sub_categories": ["nutrition", "mental health", "fitness", "science", "health"]
+        },
+        'politics': {
+            "category": "politics",
+            "nisee_category": "politics",
+            "sub_categories": ["united states", "india", "china", "russia", "europe", "asia", "africa"]
+        },
         'lifestyle': {
             "category": "lifestyle",
             "nisee_category": "lifestyle",
